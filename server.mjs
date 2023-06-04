3d1df012fba829418fe14377029d425de4e5d41d
Pious Alpha <102190049+Piouscode@users.noreply.github.com>
2023-06-04 12:42:58 +0200

    Update Server file

--- server.mjs
+++ server.mjs
@@ -30,9 +30,9 @@ app.use(notFoundHandler);
 // Mount the error handler at the end of the middleware chain
 app.use(errorHandler);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//     console.log(`Server listening on port ${port}`);
+// });
 
 export default app;
